b60bace7a5306c520410140579fb93d478ae8156
eng42-H2P <engeis@outlook.com>
2025-07-07 14:03:01 -0400

    Corrected Pins
    Updated the pins so it uses tx and rx instead of 10 and 11

--- Speaker_Refined/Speaker_Refined.ino
+++ Speaker_Refined/Speaker_Refined.ino
@@ -1,31 +1,37 @@
-#include "Arduino.h"
-#include "DFRobotDFPlayerMini.h"
-#include <SoftwareSerial.h>
-SoftwareSerial softSerial(/*rx =*/10, /*tx =*/11);
-#define FPSerial softSerial
+/*
+0001.mp3 - Welcome to BattleBop
+0002.mp3 - Get Ready
+0003.mp3 - Kick It
+0004.mp3 - Punch It
+0005.mp3 - Dodge It
+0006.mp3 - You Win
+0007.mp3 - Whomp Whomp
+0008.mp3 - Game Over
+*/
 
+
+#include <DFRobotDFPlayerMini.h>
 
 DFRobotDFPlayerMini myDFPlayer;
-void printDetail(uint8_t type, int value);
 
 void setup()
 {
-  FPSerial.begin(9600);
-  
-  if (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */true)) {  //Use serial to communicate with mp3.
-    while(true){
-      delay(0);
+  Serial.begin(9600);
+  delay(1000); // Let DFPlayer power up
+
+  if (!myDFPlayer.begin(Serial, false, true)) {
+    // Can't use Serial.println here without interfering
+    while (true); // Hang if DFPlayer doesn't start
   }
-  Serial.println(F("DFPlayer Mini online."));
-  
-  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
+
+  myDFPlayer.volume(10);  // 0-30
+  delay(2000);
 }
 
 void loop()
 {
-    myDFPlayer.play(1);
-    delay (5000);
-  
-    myDFPlayer.playMp3Folder(2);
-    delay (5000);
+  for(int i = 1; i<9; i++){
+    myDFPlayer.playMp3Folder(i);
+    delay (4000);
+    }
 }
