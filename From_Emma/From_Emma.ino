d9bb4de31ecf90fcee1e69fc30b3a9301efa2d05
MitchMcKain <mtm120@pitt.edu>
2025-07-14 14:33:00 -0400

    final code

--- From_Emma/From_Emma.ino
+++ From_Emma/From_Emma.ino
@@ -35,6 +35,8 @@ bool initialized_for_test = false;
 #define BLUE    0x001F
 #define RED     0xF800
 #define GREEN   0x07E0
+#define MAGENTA 0xF81F
+#define CYAN    0x07FF
 
 
 void setup() {
@@ -52,7 +54,7 @@ void setup() {
   
   mylcd.Init_LCD();
   mylcd.Fill_Screen(BLACK);
-  mylcd.Set_Rotation(1);
+  mylcd.Set_Rotation(3);
   mylcd.Set_Text_Mode(0);
   mylcd.Set_Text_Back_colour(BLACK);
   mylcd.Set_Text_Size(8);
@@ -141,22 +143,17 @@ void start_turn(){
   int num = random(1,4);
   if (num == 1){
     // Play audio of "Kick It"
-    print_to_screen(num);
     myDFPlayer.playMp3Folder(3);
-    
     delay(500);
   }
   else if (num == 2){
     // Play audio of "Dodge It"
-    print_to_screen(num);
     myDFPlayer.playMp3Folder(5);
     delay(500);
   }
   else{
     // Play audio of "Punch It"
-    print_to_screen(num);
     myDFPlayer.playMp3Folder(4);
-    
     delay(500);
   }
 
@@ -201,7 +198,6 @@ void start_turn(){
 
   if (polling_inputs && millis() - round_start > round_time){ // time expired
     again = process_input(0, num);
-    myDFPlayer.playMp3Folder(8);
     initialized_for_test = false;
   }
 
@@ -226,7 +222,7 @@ bool process_input(int response, int prompt){
     delay(500);
     digitalWrite(hex_increment, LOW);
     digitalWrite(green_led, LOW);
-    // display image on lcd
+    print_to_screen(prompt);// display image on lcd
     round_time -= 20; // decrease round time by 20ms
     round_start = 0; // reset round start time stamp
     score++; // Increase the user's score
@@ -237,9 +233,7 @@ bool process_input(int response, int prompt){
     victory();
   }
 
-  else if (response != prompt){ // User chose incorrectly
-    // Display LCD of losing
-    
+  else if (response != prompt){ // User chose incorrectly    
     myDFPlayer.playMp3Folder(7);
     delay(1200);
     print_to_screen(4);
@@ -250,8 +244,6 @@ bool process_input(int response, int prompt){
     delay(200);
     digitalWrite(red_led,LOW);
     delay(200);
-    
-    
     }
     
   }
@@ -262,11 +254,9 @@ bool process_input(int response, int prompt){
 }
 
 void victory(){
-  // Play audio of "You Won"
-    myDFPlayer.playMp3Folder(6);
-    delay(1000);
+    myDFPlayer.playMp3Folder(6); // Play audio of "You Won"
+    print_to_screen(6); // Show victory screen
   for (int i = 0; i < 12; i++){
-    // Display LCD of winning
     digitalWrite(green_led, HIGH);
     delay(200);
     digitalWrite(green_led,LOW);
@@ -277,6 +267,7 @@ void victory(){
 }
 
 void reset_game(){
+  print_to_screen(7);
   if (!initialized_for_test){
     // Reset hex
     digitalWrite(hex_reset, HIGH);
@@ -305,33 +296,98 @@ void print_to_screen(int i){
   //display 1 times string
   mylcd.Fill_Screen(0x0000);
   
-  if(i == 1){
-    mylcd.Set_Text_colour(RED);
-    mylcd.Print_String(" Kick It", 0, 0);
-  }
-  else if( i ==2){
-    mylcd.Set_Text_colour(GREEN);
-    mylcd.Print_String(" Dodge It", 0, 0);
-  }
-  else if(i == 3){
+  if(i == 1){ // kick
     mylcd.Set_Text_colour(BLUE);
-    mylcd.Print_String(" Punch It", 0, 0);
+    mylcd.Set_Text_Back_colour(BLACK);
+    mylcd.Set_Text_Size(8);
+    mylcd.Print_String(" O", 0, 0);
+    mylcd.Print_String("/|\\", 0, 60);
+    mylcd.Print_String("/ \\_____", 0, 120);
+    mylcd.Set_Text_colour(RED);
+    mylcd.Print_String(" O", 350, 0);
+    mylcd.Print_String("/|\\", 350, 60);
+    mylcd.Print_String("/ \\", 350, 120);
+    mylcd.Set_Text_Size(10);
+    mylcd.Set_Text_colour(MAGENTA);
+    mylcd.Set_Text_Back_colour(CYAN);
+    mylcd.Print_String(" THWAP!  ", 0, 245);
+  }
+
+  else if( i ==2){ // dodge
+    mylcd.Set_Text_colour(BLUE);
+    mylcd.Set_Text_Back_colour(BLACK);
+    mylcd.Set_Text_Size(8);
+    mylcd.Print_String(" O", 0, 65);
+    mylcd.Print_String("/ \\", 0, 125);
+    mylcd.Set_Text_colour(RED);
+    mylcd.Print_String("------ O", 50, 0);
+    mylcd.Print_String(" |\\", 340, 60);
+    mylcd.Print_String("/ \\", 340, 120);
+    mylcd.Set_Text_Size(10);
+    mylcd.Set_Text_colour(MAGENTA);
+    mylcd.Set_Text_Back_colour(CYAN);
+    mylcd.Print_String(" WOOSH!   ", 0, 245);
+  }
+
+  else if(i == 3){ // punch
+    mylcd.Set_Text_colour(BLUE);
+    mylcd.Set_Text_Back_colour(BLACK);
+    mylcd.Set_Text_Size(8);
+    mylcd.Print_String(" O", 0, 0);
+    mylcd.Print_String("/|----------", 0, 60);
+    mylcd.Print_String("/ \\", 0, 120);
+    mylcd.Set_Text_colour(RED);
+    mylcd.Print_String(" O", 350, 0);
+    mylcd.Print_String("/|\\", 350, 60);
+    mylcd.Print_String("/ \\", 350, 120);
+    mylcd.Set_Text_Size(10);
+    mylcd.Set_Text_colour(MAGENTA);
+    mylcd.Set_Text_Back_colour(CYAN);
+    mylcd.Print_String("  POW!  ", 0, 245);
   }
   
-  else if(i ==4){
+  else if(i ==4){ // game over
     mylcd.Set_Text_colour(RED);
+    mylcd.Set_Text_Back_colour(BLACK);
     mylcd.Set_Text_Size(15);
-    mylcd.Print_String("GAME", 75, 40);
-    mylcd.Print_String("OVER", 75, 160);
+    mylcd.Print_String("GAME", 75, 45);
+    mylcd.Print_String("OVER", 75, 165);
     mylcd.Set_Text_Size(8);
-
   }
-  else if(i==5){
+
+  else if(i==5){ // start screen
     mylcd.Set_Text_colour(BLUE);
     mylcd.Set_Text_Size(15);
-    mylcd.Print_String("PRESS", 30, 40);
-    mylcd.Print_String("PLAY", 65, 160);
+    mylcd.Print_String("PRESS", 30, 45);
+    mylcd.Print_String("PLAY", 65, 165);
     mylcd.Set_Text_Size(8);
+  }
+
+  else if (i == 6){ // victory
+    mylcd.Set_Text_colour(GREEN);
+    mylcd.Set_Text_Back_colour(BLACK);
+    mylcd.Set_Text_Size(15);
+    mylcd.Print_String("YOU", 125, 45);
+    mylcd.Print_String("WIN", 125, 165);
+    mylcd.Set_Text_Size(8);
+  }
+
+  else if (i == 7){ // idle
+    mylcd.Fill_Screen(0x0000);
+    mylcd.Set_Text_colour(BLUE);
+    mylcd.Set_Text_Back_colour(BLACK);
+    mylcd.Set_Text_Size(8);
+    mylcd.Print_String(" O", 0, 0);
+    mylcd.Print_String("/|\\", 0, 60);
+    mylcd.Print_String("/ \\", 0, 120);
+    mylcd.Set_Text_colour(RED);
+    mylcd.Print_String(" O", 350, 0);
+    mylcd.Print_String("/|\\", 350, 60);
+    mylcd.Print_String("/ \\", 350, 120);
+    mylcd.Set_Text_Size(10);
+    mylcd.Set_Text_colour(MAGENTA);
+    mylcd.Set_Text_Back_colour(CYAN);
+    mylcd.Print_String(" FIGHT!  ", 0, 245);
   }
   
 
